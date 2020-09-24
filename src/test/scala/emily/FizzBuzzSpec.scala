320a665332609381151b0f9ed3be9978cbacc74c
EWright212 <emilywright212@gmail.com>
2020-09-24 20:16:21 +0100

    Refactoring remove duplicated code

--- src/test/scala/emily/FizzBuzzSpec.scala
+++ src/test/scala/emily/FizzBuzzSpec.scala
@@ -5,24 +5,21 @@ import org.scalatest.matchers.should
 
 
 class FizzBuzzSpec extends AnyFlatSpec with should.Matchers {
+  var FizzBuzz = new FizzBuzz
 
   "FizzBuzz" should "return an ordinary number" in {
-    var FizzBuzz = new FizzBuzz
     FizzBuzz.convert(1) should be("1")
     FizzBuzz.convert(2) should be("2")
   }
   "FizzBuzz" should "return Fizz for multiples of three" in {
-    var FizzBuzz = new FizzBuzz
     FizzBuzz.convert(3) should be("Fizz")
     FizzBuzz.convert(6) should be("Fizz")
   }
   "FizzBuzz" should "return Buzz for multiples of five" in {
-    var FizzBuzz = new FizzBuzz
     FizzBuzz.convert(5) should be("Buzz")
     FizzBuzz.convert(10) should be("Buzz")
   }
   "FizzBuzz" should "return FizzBuzz for multiples of fifteen" in {
-    var FizzBuzz = new FizzBuzz
     FizzBuzz.convert(15) should be("FizzBuzz")
     FizzBuzz.convert(30) should be("FizzBuzz")
   }
