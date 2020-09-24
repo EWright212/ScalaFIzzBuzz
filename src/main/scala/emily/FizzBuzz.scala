047c16e7c4293eecc5ee5571a148d77d19ee8617
EWright212 <emilywright212@gmail.com>
2020-09-24 20:14:28 +0100

    FizzBuzz converter returns Buzz for multiples of fifteen

--- src/main/scala/emily/FizzBuzz.scala
+++ src/main/scala/emily/FizzBuzz.scala
@@ -2,14 +2,15 @@ package emily
 
 class FizzBuzz {
   def convert(number: Int): String = {
-    if (number % 3 == 0) {
-      "Fizz"
-    } else if(number % 5 == 0){
+    if (number % 15 == 0) {
+      "FizzBuzz"
+    } else if (number % 5 == 0) {
       "Buzz"
+    } else if (number % 3 == 0) {
+      "Fizz"
     } else {
       number.toString
     }
-
   }
 
 }
--- src/test/scala/emily/FizzBuzzSpec.scala
+++ src/test/scala/emily/FizzBuzzSpec.scala
@@ -11,15 +11,19 @@ class FizzBuzzSpec extends AnyFlatSpec with should.Matchers {
     FizzBuzz.convert(1) should be("1")
     FizzBuzz.convert(2) should be("2")
   }
-  "FizzBuzz" should "return an Fizz for multiples of three" in {
+  "FizzBuzz" should "return Fizz for multiples of three" in {
     var FizzBuzz = new FizzBuzz
     FizzBuzz.convert(3) should be("Fizz")
     FizzBuzz.convert(6) should be("Fizz")
   }
-  "FizzBuzz" should "return an Buzz for multiples of five" in {
+  "FizzBuzz" should "return Buzz for multiples of five" in {
     var FizzBuzz = new FizzBuzz
     FizzBuzz.convert(5) should be("Buzz")
     FizzBuzz.convert(10) should be("Buzz")
+  }
+  "FizzBuzz" should "return FizzBuzz for multiples of fifteen" in {
+    var FizzBuzz = new FizzBuzz
+    FizzBuzz.convert(15) should be("FizzBuzz")
   }
 
 }
