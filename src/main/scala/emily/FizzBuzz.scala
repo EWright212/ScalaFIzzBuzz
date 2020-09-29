2336787125dc83b2d8a2e69a74779aa444d3acbb
EWright212 <emilywright212@gmail.com>
2020-09-29 18:12:24 +0100

    Refactor fizzbuzz to add fizz and buzz to an empty string to improve efficiency

--- src/main/scala/emily/FizzBuzz.scala
+++ src/main/scala/emily/FizzBuzz.scala
@@ -2,15 +2,17 @@ package emily
 
 class FizzBuzz {
   def convert(number: Int): String = {
-    if (number % 15 == 0) {
-      "FizzBuzz"
-    } else if (number % 5 == 0) {
-      "Buzz"
-    } else if (number % 3 == 0) {
-      "Fizz"
-    } else {
-      number.toString
+    var output = ""
+    if (number % 3 == 0) {
+      output+= "Fizz"
     }
+    if (number % 5 == 0) {
+      output += "Buzz"
+    }
+    if (output == "") {
+      output = number.toString
+    }
+    output
   }
 
 }
